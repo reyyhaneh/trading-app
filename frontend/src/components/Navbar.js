d3065cd02ce4e2ce99ab19f902ecf1cea5241dbb
reyyhaneh <reyhoondavaee@gmail.com>
2025-05-30 19:04:19 +0330

    auto trading button added to navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -21,7 +21,6 @@ const Navbar = () => {
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">TradingApp</span>
           </Link>
-
           {/* Hamburger Menu Button */}
           <button
             data-collapse-toggle="navbar-default"
@@ -81,6 +80,14 @@ const Navbar = () => {
                       className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                     >
                       Profile
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/auto-trading-rules"
+                      className="block px-4 py-1.5 border border-blue-500 text-blue-500 rounded-full hover:bg-blue-500 hover:text-white transition duration-200"
+                    >
+                      Auto Trading
                     </Link>
                   </li>
                   <li>
